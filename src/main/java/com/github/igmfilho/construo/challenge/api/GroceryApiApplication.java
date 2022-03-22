81df9c3c16ca641763a5b24e59c2df938fff5d4c
Ivan Filho <igmfilho@gmail.com>
2022-03-22 06:47:01 +0000

    [Spring initializr] first source code was generated by start.spring.io

---
+++ src/main/java/com/github/igmfilho/construo/challenge/api/GroceryApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.igmfilho.construo.challenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/igmfilho/construo/challenge/api/GroceryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.igmfilho.construo.challenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
